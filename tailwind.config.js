9363fea4558b97902ac7f05de7f5381fe530ab54
jitsbenPC <jitsbenPC@gmail.com>
2026-05-01 12:23:33 -0500

    prueba dee estructura de carpetas y archivos
    se ingreso codigo, para verificar q tailwind si esta agarrando con index

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'checa-blue': '#2b7bc5',    
+                'checa-light-blue': '#4a90e2',
+                'checa-green': '#d4edda',    
+                'checa-green-text': '#155724',
+                'checa-red': '#f8d7da',      
+                'checa-red-text': '#721c24'   
+            }
+        }
+    }
+};
